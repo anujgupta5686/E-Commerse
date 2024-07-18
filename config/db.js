2a3e7491d3f698e1b26daa9431037bb559a47739
anujgupta5686 <anujgupta5686@gmail.com>
2024-07-18 11:20:47 +0530

    change in the DB confir

--- config/db.js
+++ config/db.js
@@ -1,27 +1,18 @@
-// const mongoose = require("mongoose")
-
-// async function connectDB(){
-//     try{
-//         await mongoose.connect(process.env.MONGODB_URI)
-//     }catch(err){
-//         console.log(err)
-//     }
-// }
-
-// module.exports = connectDB
-
 const mongoose = require("mongoose");
 require("dotenv").config();
 
 const connectDB = async () => {
-  await mongoose
-    .connect(process.env.MONGODB_URI)
-    .then(() => console.log("DB Connected Successfully"))
-    .catch((error) => {
-      console.log("DB Connection Failed");
-      console.error(error);
-      process.exit(1);
+  try {
+    await mongoose.connect(process.env.MONGODB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
     });
+    console.log("DB Connected Successfully");
+  } catch (error) {
+    console.log("DB Connection Failed");
+    console.error(error);
+    process.exit(1);
+  }
 };
 
 module.exports = connectDB;
